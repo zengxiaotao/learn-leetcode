ff02ae77a49a50c4806729313c458cb84ec0c99c
xiaotao.zeng01 <xiaotao.zeng01@bianlifeng.com>
2019-02-26 21:05:52 +0800

    add 287

---
+++ 287.find-the-duplicate-number.js
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=287 lang=javascript
+ *
+ * [287] Find the Duplicate Number
+ *
+ * https://leetcode.com/problems/find-the-duplicate-number/description/
+ *
+ * algorithms
+ * Medium (48.25%)
+ * Total Accepted:    165.2K
+ * Total Submissions: 342.3K
+ * Testcase Example:  '[1,3,4,2,2]'
+ *
+ * Given an array nums containing n + 1 integers where each integer is between
+ * 1 and n (inclusive), prove that at least one duplicate number must exist.
+ * Assume that there is only one duplicate number, find the duplicate one.
+ *
+ * Example 1:
+ *
+ *
+ * Input: [1,3,4,2,2]
+ * Output: 2
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: [3,1,3,4,2]
+ * Output: 3
+ *
+ * Note:
+ *
+ *
+ * You must not modify the array (assume the array is read only).
+ * You must use only constant, O(1) extra space.
+ * Your runtime complexity should be less than O(n2).
+ * There is only one duplicate number in the array, but it could be repeated
+ * more than once.
+ *
+ *
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function (nums) {
+  const len = nums.length;
+  const set = new Set();
+  for (let i = 0; i < len; i += 1) {
+    if (set.has(nums[i])) return nums[i];
+    set.add(nums[i]);
+  }
+};
